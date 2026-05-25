07e7ffd14e7da1f566888bdfacf21a41719b9cc4
Lenz Gschwendtner <mail@norbu09.org>
2026-05-25 19:51:30 +0100

    fix asdf fish integration for Arch AUR package

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -4,6 +4,14 @@ function fish_greeting
 end
 
 # asdf version manager
-source ~/.asdf/asdf.fish
+if test -z "$ASDF_DATA_DIR"
+    set _asdf_shims "$HOME/.asdf/shims"
+else
+    set _asdf_shims "$ASDF_DATA_DIR/shims"
+end
+if not contains $_asdf_shims $PATH
+    set -gx --prepend PATH $_asdf_shims
+end
+set --erase _asdf_shims
 
 starship init fish | source
